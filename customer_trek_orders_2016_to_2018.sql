e67d15ee769018f4fd2ad68bdb26a63a32a3224d
Michael Vickers <mjvickers00@gmail.com>
2025-01-23 19:19:48 -0800

    UPDATED - Customer Orders (2016-2018) - Filtered by Orders with 'Trek' products
    queried fields: customer_id, full_name (concatenated field), state, total_trek_orders, trek_revenue
    
    techniques: subquery (used for filtering in the WHERE clause), aggregate functions (COUNT, SUM), concatenate (used to combine two fields), EXTRACT (year from order date) - filtering, full-text search - filtering, INNER JOIN (matches only)

--- customer_trek_orders_2016_to_2018.sql
+++ customer_trek_orders_2016_to_2018.sql
@@ -7,9 +7,9 @@ SELECT
 FROM customers
 INNER JOIN orders ON orders.customer_id = customers.customer_id
 INNER JOIN order_items AS oi ON oi.order_id = orders.order_id
-WHERE oi.order_id IN (
+WHERE oi.product_id IN (
 	SELECT
-		order_id
+		oi.product_id
 	FROM order_items AS oi
 	INNER JOIN products ON products.product_id = oi.product_id
 	INNER JOIN brands ON brands.brand_id = products.brand_id
