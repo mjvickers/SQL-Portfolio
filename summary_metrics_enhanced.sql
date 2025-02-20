292a5b6d8dda0597ee3329a0aa8152dc4d7246a4
Michael Vickers <mjvickers00@gmail.com>
2025-02-20 09:49:23 -0800

    Summary Metrics Enhanced
    *new fields added*
    fields queried: year, postseason, team, conf, seed, wab, barthag, adjoe, adjde, efg_o, 3p_o, tor, orb, drb
    
    techniques: JOINs

---
+++ summary_metrics_enhanced.sql
@@ -0,0 +1,29 @@
+SELECT
+	year,
+	postseason,
+	team,
+	conf,
+	seed,
+	wab,
+	barthag,
+	adjoe,
+	adjde,
+	efg_o,
+	"3p_o",
+	tor,
+	orb,
+	drb
+FROM teams
+INNER JOIN conferences ON conferences.team_id = teams.team_id
+INNER JOIN seasons ON seasons.season_id = conferences.season_id
+INNER JOIN postseason ON postseason.season_id = seasons.season_id
+	AND postseason.team_id = teams.team_id
+	AND postseason.team_id = conferences.team_id
+INNER JOIN metrics ON metrics.season_id = seasons.season_id
+	AND metrics.season_id = postseason.season_id
+	AND metrics.team_id = teams.team_id
+	AND metrics.team_id = conferences.team_id
+	AND metrics.team_id = postseason.team_id
+WHERE postseason IS NOT NULL
+ORDER BY year ASC, seed ASC;
+	
